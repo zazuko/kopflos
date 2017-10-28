5de314cd246970bb0fbf18bcbafa4e09d5d1db72
bergi <bergi@axolotlfarm.org>
2017-10-29 00:41:15 +0200

    changed example to load also from http URL

--- examples/spaceprobes.js
+++ examples/spaceprobes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const hydraView = require('..')
+const path = require('path')
 
 const app = express()
 
-hydraView.fromJsonLdFile('/api', 'spaceprobes.api.json', {
+hydraView.fromUrl('/api', 'file://' + path.join(__dirname, 'spaceprobes.api.jsonld'), {
+// hydraView.fromUrl('/api', 'http://localhost:8080/spaceprobes.api.jsonld', {
   debug: true,
   sparqlEndpointUrl: 'https://query.wikidata.org/bigdata/namespace/wdq/sparql',
   contextHeader: '/context/'
