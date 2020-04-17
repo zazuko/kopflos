23f6f048146121eaf24ca43dbbda807697728c6a
bergi <bergi@axolotlfarm.org>
2020-04-18 00:31:37 +0200

    fixed namespace error from rebase, wait for init of Api in middleware

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const debug = require('debug')('hydra-box:middleware')
 const absoluteUrl = require('absolute-url')
 const { Router } = require('express')
+const { defer } = require('promise-the-world')
 const rdf = { ...require('@rdfjs/data-model'), ...require('@rdfjs/dataset') }
 const rdfHandler = require('@rdfjs/express-handler')
 const setLink = require('set-link')
@@ -8,8 +9,10 @@ const apiHeader = require('./lib/middleware/apiHeader')
 const iriTemplate = require('./lib/middleware/iriTemplate')
 const operation = require('./lib/middleware/operation')
 const resource = require('./lib/middleware/resource')
+const waitFor = require('./lib/middleware/waitFor')
 
 function middleware (api, store, { baseIriFromRequest } = {}) {
+  const init = defer()
   const router = new Router()
 
   router.use(absoluteUrl())
@@ -39,15 +42,24 @@ function middleware (api, store, { baseIriFromRequest } = {}) {
       debug(`api.term was not set. Will use: ${api.term.value}`)
     }
 
-    await api.init()
+    try {
+      await api.init()
 
-    next()
+      init.resolve()
+
+      next()
+    } catch (err) {
+      init.reject(err)
+
+      next(err)
+    }
   })
+
   router.use(rdfHandler({ baseIriFromRequest }))
-  router.use(apiHeader(api))
-  router.use(iriTemplate(api))
+  router.use(waitFor(init, () => apiHeader(api)))
+  router.use(waitFor(init, () => iriTemplate(api)))
   router.use(resource(store))
-  router.use(operation(api))
+  router.use(waitFor(init, () => operation(api)))
 
   return router
 }
