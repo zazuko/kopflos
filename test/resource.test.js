27d381425823022f1a818a73951301f16338e6da
tpluscode <tpluscode@users.noreply.github.com>
2020-05-12 09:45:27 +0200

    fix: return 404 when loader does not find a resource

---
+++ test/resource.test.js
@@ -0,0 +1,42 @@
+const { strictEqual } = require('assert')
+const express = require('express')
+const { describe, it, beforeEach } = require('mocha')
+const sinon = require('sinon')
+const request = require('supertest')
+const RDF = require('@rdfjs/data-model')
+const resource = require('../lib/middleware/resource')
+
+describe('middleware/resource', () => {
+  let loader, forClassOperation, forPropertyOperation
+
+  beforeEach(() => {
+    forClassOperation = sinon.stub().resolves([])
+    forPropertyOperation = sinon.stub().resolves([])
+
+    loader = {
+      forClassOperation,
+      forPropertyOperation
+    }
+  })
+
+  function hydraMock(req, res, next) {
+    req.hydra = {
+      term: RDF.namedNode(req.uri)
+    }
+    next()
+  }
+
+  it('calls next middleware when resource is not found', async () => {
+    // given
+    const app = express()
+    const handler = resource({ loader })
+    app.use(hydraMock)
+    app.use(handler)
+
+    // when
+    const response = await request(app).get('/')
+
+    // then
+    strictEqual(response.status, 404)
+  })
+})
