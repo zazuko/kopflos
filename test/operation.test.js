74075e23f982c035ebad98054bd270d5a0da33b4
tpluscode <tpluscode@users.noreply.github.com>
2020-08-17 08:01:45 +0200

    fix: improve operations selection for property usage

--- test/operation.test.js
+++ test/operation.test.js
@@ -32,11 +32,15 @@ describe('middleware/operation', () => {
       })
   })
 
-  function hydraMock(resource, rootResource) {
+  function hydraMock ({ types = [], term, dataset = RDF.dataset() } = {}, rootResource) {
     return function (req, res, next) {
       req.hydra = {
-        term: RDF.namedNode(rootResource || req.url),
-        resource,
+        term: RDF.namedNode(req.url),
+        resource: {
+          dataset,
+          term: rootResource ? RDF.namedNode(rootResource) : term || RDF.namedNode(req.url),
+          types
+        }
       }
       next()
     }
@@ -45,21 +49,24 @@ describe('middleware/operation', () => {
   it('calls next middleware when no resource was loaded', async () => {
     // given
     const app = express()
-    app.use(hydraMock())
+    app.use((req, res, next) => {
+      req.hydra = {}
+      next()
+    })
     app.use(middleware(api))
 
     // when
     const response = await request(app).get('/')
 
     // then
-    assert.strictEqual(response.status,404)
+    assert.strictEqual(response.status, 404)
   })
 
-  it('calls next middleware when no operation is found', async () => {
+  it('calls next middleware when class is not supported', async () => {
     // given
     const app = express()
     app.use(hydraMock({
-      types: [NS.NoSuchClass],
+      types: [NS.NoSuchClass]
     }))
     app.use(middleware(api))
 
@@ -67,14 +74,29 @@ describe('middleware/operation', () => {
     const response = await request(app).get('/')
 
     // then
-    assert.strictEqual(response.status,404)
+    assert.strictEqual(response.status, 404)
+  })
+
+  it('calls next middleware when no operation is found', async () => {
+    // given
+    const app = express()
+    app.use(hydraMock({
+      types: [NS.Person]
+    }))
+    app.use(middleware(api))
+
+    // when
+    const response = await request(app).patch('/')
+
+    // then
+    assert.strictEqual(response.status, 404)
   })
 
   it('calls GET handler when HEAD is requested', async () => {
     // given
     const app = express()
     app.use(hydraMock({
-      types: [NS.Person],
+      types: [NS.Person]
     }))
     app.use(middleware(api))
 
@@ -82,7 +104,7 @@ describe('middleware/operation', () => {
     const response = await request(app).head('/')
 
     // then
-    assert.strictEqual(response.status,200)
+    assert.strictEqual(response.status, 200)
     assert(api.loaderRegistry.load.calledOnceWith(
       sinon.match.hasNested('term.value', sinon.match(/person-get$/)),
       sinon.match.any
@@ -99,7 +121,7 @@ describe('middleware/operation', () => {
     app.use(hydraMock({
       types: [NS.Person],
       term: '/friends',
-      dataset,
+      dataset
     }, '/john-doe'))
     app.use(middleware(api))
 
@@ -107,10 +129,84 @@ describe('middleware/operation', () => {
     const response = await request(app).post('/friends')
 
     // then
-    assert.strictEqual(response.status,200)
+    assert.strictEqual(response.status, 200)
     assert(api.loaderRegistry.load.calledOnceWith(
       sinon.match.hasNested('term.value', sinon.match(/friends-post$/)),
       sinon.match.any
     ))
+  })
+
+  it('does not call supported property operation when resource does not match', async () => {
+    // given
+    const app = express()
+    const dataset = RDF.dataset()
+    clownface({ dataset })
+      .namedNode('/john-doe')
+      .addOut(NS.friends, RDF.namedNode('/friends'))
+    app.use(hydraMock({
+      types: [NS.Person],
+      term: '/friends',
+      dataset
+    }, '/john-doe'))
+    app.use(middleware(api))
+
+    // when
+    const response = await request(app).post('/john-doe')
+
+    // then
+    assert.strictEqual(response.status, 404)
+    assert(api.loaderRegistry.load.notCalled)
+  })
+
+  it('throws when multiple operations are found for class', async () => {
+    // given
+    const app = express()
+    app.use(hydraMock({
+      types: [NS.Person]
+    }))
+    app.use(middleware(api))
+
+    // when
+    const response = await request(app).delete('/foo')
+
+    // then
+    assert.strictEqual(response.status, 500)
+  })
+
+  it('throws when multiple operations are found for property object', async () => {
+    // given
+    const app = express()
+    const dataset = RDF.dataset()
+    clownface({ dataset })
+      .namedNode('/john-doe')
+      .addOut(NS.interests, RDF.namedNode('/john-doe/interests'))
+    app.use(hydraMock({
+      types: [NS.Person],
+      term: '/john-doe/interests',
+      dataset
+    }, '/john-doe'))
+    app.use(middleware(api))
+
+    // when
+    const response = await request(app).get('/john-doe/interests')
+
+    // then
+    assert.strictEqual(response.status, 500)
+  })
+
+  it('throws when operation fails to load', async () => {
+    // given
+    const app = express()
+    app.use(hydraMock({
+      types: [NS.Person]
+    }))
+    app.use(middleware(api))
+
+    // when
+    api.loaderRegistry.load.returns(null)
+    const response = await request(app).get('/john-doe')
+
+    // then
+    assert.strictEqual(response.status, 500)
   })
 })
