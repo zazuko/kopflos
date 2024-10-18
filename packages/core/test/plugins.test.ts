0614fb5beb01291d35bd23ee819db40690afcd7b
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-10-18 12:30:48 +0200

    feat(express middleware): support named exports

--- packages/core/test/plugins.test.ts
+++ packages/core/test/plugins.test.ts
@@ -1,3 +1,4 @@
+import url from 'node:url'
 import { expect } from 'chai'
 import { loadPlugins } from '../plugins.js'
 
@@ -9,6 +10,31 @@ describe('@kopflos-cms/core/plugins.js', () => {
 
       // then
       expect(plugins).to.have.length(1)
+    })
+
+    it('can explicitly disable plugins', async () => {
+      // when
+      const plugins = await loadPlugins({
+        '@kopflos-cms/core/plugin/shorthandTerms.js': false,
+      })
+
+      // then
+      expect(plugins).to.have.length(0)
+    })
+
+    it('allows referencing named exports', async () => {
+      // given
+      const pluginPath = url.fileURLToPath(
+        new URL('./support/plugin.js', import.meta.url),
+      ) + '#namedPlugin'
+
+      // when
+      const plugins = await loadPlugins({
+        [pluginPath]: 'foobar',
+      })
+
+      // then
+      expect(plugins[1]).to.have.property('name', 'foobar')
     })
   })
 })
