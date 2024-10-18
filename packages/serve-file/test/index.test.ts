073dc7e0c92bd86109d7736a0d517b8e80fd4dea
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-10-18 15:32:13 +0200

    feat(serve-file): override content type

---
+++ packages/serve-file/test/index.test.ts
@@ -0,0 +1,80 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ReadableStream } from 'node:stream/web'
+import { Readable } from 'node:stream'
+import { expect } from 'chai'
+import type { ResultEnvelope } from '@kopflos-cms/core'
+import getStream from 'get-stream'
+import factory from '../index.js'
+
+describe('@kopflos-cms/serve-file', () => {
+  it('serves from path', async () => {
+    // given
+    const serveFile = factory('test/test.txt')
+
+    // when
+    const response = await serveFile({} as any)
+
+    // then
+    expect(response).to.deep.equal({
+      status: 200,
+      body: 'Hello World\n',
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    })
+  })
+
+  it('detects media type', async () => {
+    // given
+    const serveFile = factory('test/test.json')
+
+    // when
+    const response = await serveFile({} as any)
+
+    // then
+    expect(response).to.deep.contain({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  })
+
+  it('allows media type override', async () => {
+    // given
+    const serveFile = factory({
+      path: 'test/test.json',
+      contentType: 'application/ld+json',
+    })
+
+    // when
+    const response = await serveFile({} as any)
+
+    // then
+    expect(response).to.deep.contain({
+      headers: {
+        'Content-Type': 'application/ld+json',
+      },
+    })
+  })
+
+  it('serves stream', async () => {
+    // given
+    const serveFile = factory({
+      path: 'test/test.txt',
+      stream: true,
+    })
+
+    // when
+    const response = await serveFile({} as any) as ResultEnvelope
+
+    // then
+    expect(response).to.deep.contain({
+      status: 200,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    })
+    expect(response.body).to.be.instanceOf(ReadableStream)
+    await expect(getStream(Readable.fromWeb(response.body as any))).to.eventually.equal('Hello World\n')
+  })
+})
