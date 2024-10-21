c5610d52d9625bd6961ed5c724a2f383e377cff1
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-10-21 14:45:17 +0200

    test(cli): config discovery

---
+++ packages/cli/test/lib/config.test.ts
@@ -0,0 +1,33 @@
+import url from 'node:url'
+import { expect } from 'chai'
+import { loadConfig } from '../../lib/config.js'
+
+describe('kopflos/lib/config.js', () => {
+  describe('loadConfig', () => {
+    it('should discover the config file', async () => {
+      const config = await loadConfig({
+        path: undefined,
+        root: url.fileURLToPath(new URL('..', import.meta.url)),
+      })
+
+      expect(config).to.be.deep.equal({
+        baseIri: 'https://example.com/',
+      })
+    })
+
+    it('should load config from path', async () => {
+      // given
+      const configPath = url.fileURLToPath(new URL('../fixtures/config.json', import.meta.url))
+
+      // when
+      const config = await loadConfig({
+        path: configPath,
+      })
+
+      // given
+      expect(config).to.be.deep.equal({
+        baseIri: 'https://example.com/',
+      })
+    })
+  })
+})
