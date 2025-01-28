227ed3d406a9abdd28f18789fd96ab29d485c3c9
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2025-01-28 09:48:59 +0100

    fix spelling

--- packages/core/handlers.ts
+++ packages/core/handlers.ts
@@ -1,7 +1,7 @@
 import { log, type SubjectHandler } from './index.js'
 
 export function getCoreRepresentation(): SubjectHandler {
-  log.warn('Using @kopflos-cms/handlers.js#getCoreRepresentation is not equivalent to skipping the handler altogether because it materiliazes the dataset. This is not recommended for large resources.')
+  log.warn('Using @kopflos-cms/core/handlers.js#getCoreRepresentation is not equivalent to skipping the handler altogether because it materiliazes the dataset. This is not recommended for large resources.')
 
   return ({ subject }) => {
     return {
