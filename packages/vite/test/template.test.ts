ecba9499c89a14716791651157695391f6664dfb
tpluscode <tpluscode@users.noreply.github.com>
2025-12-30 16:52:13 +0100

    test: transforming template

--- packages/vite/test/template.test.ts
+++ packages/vite/test/template.test.ts
@@ -3,26 +3,65 @@ import { expect } from 'chai'
 import type { HandlerArgs, SubjectHandler } from '@kopflos-cms/core'
 import { createEnv } from '@kopflos-cms/core/env.js' // eslint-disable-line import/no-unresolved
 import Kopflos from '@kopflos-cms/core'
+import rdf from '@zazuko/env-node'
 import { transform } from '../template.js'
 
 describe('@kopflos-cms/vite/template.js', function () {
   let handler: SubjectHandler
 
-  before(function () {
-    const kopflos = new Kopflos({
-      baseIri: 'http://example.com/',
-      sparql: {
-        default: 'http://example.com/sparql',
-      },
-      plugins: {
-        '@kopflos-cms/vite': {
-        },
-      },
-    })
-    handler = transform.bind(kopflos)()
-  })
+  function mockPlugin() {
+    return class {
+      public readonly name = '@kopflos-cms/vite'
+
+      get viteDevServer() {
+        return {
+          transformIndexHtml() {
+            return 'transformed'
+          },
+        }
+      }
+    }
+  }
 
   describe('transform', function () {
+    describe('when vite plugin is missing', function () {
+      before(function () {
+        const kopflos = new Kopflos({
+          baseIri: 'http://example.com/',
+          sparql: {
+            default: 'http://example.com/sparql',
+          },
+        })
+        handler = transform.bind(kopflos)()
+      })
+
+      it('throws an error', async function () {
+        const context = {
+          env: createEnv({
+            baseIri: 'http://example.com/',
+            sparql: {
+              default: 'http://example.com/sparql',
+            },
+          }),
+        } as HandlerArgs
+        const response = { headers: { 'content-type': 'text/html' }, body: '' }
+
+        await expect(handler(context, response)).to.eventually.equal(response)
+      })
+    })
+
+    before(function () {
+      const kopflos = new Kopflos({
+        baseIri: 'http://example.com/',
+        sparql: {
+          default: 'http://example.com/sparql',
+        },
+      }, {
+        plugins: [mockPlugin()],
+      })
+      handler = transform.bind(kopflos)()
+    })
+
     it('throws an error if there is no previous response', async function () {
       const context = {} as HandlerArgs
 
@@ -56,6 +95,21 @@ describe('@kopflos-cms/vite/template.js', function () {
       const response = { headers: { 'content-type': 'text/html' }, body: '' }
 
       await expect(handler(context, response)).to.eventually.equal(response)
+    })
+
+    it('calls transform on the vite dev server', async function () {
+      const context = {
+        env: createEnv({
+          baseIri: 'http://example.com/',
+          sparql: {
+            default: 'http://example.com/sparql',
+          },
+        }),
+        subject: rdf.clownface({ value: 'http://example.com/page' }),
+      } as unknown as HandlerArgs
+      const response = { headers: { 'content-type': 'text/html' }, body: '' }
+
+      await expect(handler(context, response)).to.eventually.have.property('body', 'transformed')
     })
   })
 })
