a458d9fdcd0b98bb70b9ba58c4dca1cfc2e5b469
tpluscode <tpluscode@users.noreply.github.com>
2026-03-02 16:42:14 +0100

    feat(core): `appNs`

---
+++ .changeset/spotty-emus-talk.md
@@ -0,0 +1,5 @@
+---
+"@kopflos-cms/core": patch
+---
+
+Added `appNs` `NamespaceBuilder` to the environment which creates named nodes relative to app base
--- packages/core/lib/env/KopflosFactory.ts
+++ packages/core/lib/env/KopflosFactory.ts
@@ -1,22 +1,33 @@
+import type { NamespaceBuilder } from '@rdfjs/namespace'
+import type { NamespaceFactory } from '@rdfjs/namespace/Factory.js'
+import type { Environment } from '@rdfjs/environment/Environment.js'
 import type { KopflosConfig } from '../Kopflos.js'
 
 export interface KopflosFactory {
   readonly kopflos: {
     readonly config: KopflosConfig
     readonly variables: Record<string, unknown>
+    readonly appNs: NamespaceBuilder
   }
 }
 
 export default (config: KopflosConfig, basePath: string) => {
   const variables = config.variables || {}
+  let parent: Environment<NamespaceFactory> | undefined
 
   return class implements KopflosFactory {
+    init(this: Environment<NamespaceFactory>) {
+      // eslint-disable-next-line @typescript-eslint/no-this-alias
+      parent = this
+    }
+
     get kopflos() {
       return {
         basePath,
         buildDir: config.buildDir || 'dist',
         config: Object.freeze(config),
         variables,
+        appNs: parent!.namespace(config.baseIri),
       }
     }
 
