79bc2750e8bd5cb1d56f8470252d88ce49869667
tpluscode <tpluscode@users.noreply.github.com>
2026-03-05 22:03:40 +0100

    test: PageUrlTransform.test.ts

--- labs/pages/lib/PageUrlTransform.ts
+++ labs/pages/lib/PageUrlTransform.ts
@@ -1,13 +1,14 @@
 import type { TransformCallback } from 'node:stream'
 import { Transform } from 'node:stream'
-import type { KopflosEnvironment } from '@kopflos-cms/core'
-import type { Quad } from '@rdfjs/types'
+import type { DataFactory, Quad } from '@rdfjs/types'
+import type { Environment } from '@rdfjs/environment/Environment.js'
+import type NsBuildersFactory from '@tpluscode/rdf-ns-builders'
 import type { Bindings } from '../queries/page-patterns.rq'
 
-const groupRegex = /\(\?<(?<groupName>\w+)>.+\)/
+const groupRegex = /\(\?<(?<groupName>\w+)>.+?\)/
 
 export default class extends Transform {
-  constructor(private pagePatterns: Bindings[], private env: KopflosEnvironment) {
+  constructor(private pagePatterns: Bindings[], private env: Environment<NsBuildersFactory | DataFactory>) {
     super({
       objectMode: true,
     })
@@ -15,15 +16,18 @@ export default class extends Transform {
 
   _transform(chunk: Quad, encoding: BufferEncoding, callback: TransformCallback) {
     if (chunk.predicate.equals(this.env.ns.schema.mainEntityOfPage)) {
-      const { pagePattern, resourcePattern } = this.pagePatterns.find(binding => binding.pagePattern.equals(chunk.object)) ?? <Bindings>{}
-      const groups = chunk.subject.value.match(new RegExp(resourcePattern!.value))?.groups
-      const pagePatternValue: string | undefined = pagePattern?.value
-      const pageUrl = pagePatternValue?.replace(groupRegex, (_, groupName) => {
-        return groups?.[groupName] ?? ''
-      }).replace(/\$$/, '')
+      const binding = this.pagePatterns.find(binding => binding.pagePattern.equals(chunk.object))
+      if (binding) {
+        const { pagePattern, resourcePattern } = binding
+        const groups = chunk.subject.value.match(new RegExp(resourcePattern.value))?.groups
+        const pagePatternValue: string = pagePattern.value
+        const pageUrl = pagePatternValue.replace(new RegExp(groupRegex, 'g'), (_, groupName) => {
+          return groups?.[groupName] ?? ''
+        }).replace(/\$$/, '')
 
-      if (pageUrl) {
-        return callback(undefined, this.env.quad(chunk.subject, chunk.predicate, this.env.namedNode(pageUrl)))
+        if (pageUrl) {
+          return callback(undefined, this.env.quad(chunk.subject, chunk.predicate, this.env.namedNode(pageUrl)))
+        }
       }
     }
 
