5024fcbd0732e85accf0d01a212554510f64cfda
tpluscode <tpluscode@users.noreply.github.com>
2026-03-24 17:09:43 +0100

    refactor: avoid awkward typing

--- labs/pages/test/fixtures/pages/ssr-head.ts
+++ labs/pages/test/fixtures/pages/ssr-head.ts
@@ -1,13 +1,14 @@
 import { html } from 'lit'
 import { expand } from '@zazuko/prefixes'
 import type { ExecuteConstruct } from 'sparqlc'
+import env from '@zazuko/env-node'
 import type { Page } from '../../../lib/Plugin.js'
 
 export default <Page>{
   head: ({ env, data }) => `<title>${data.foo.out(env.ns.schema.title).value}</title>`,
   body: () => html`<div>Body</div>`,
   queries: {
-    foo: (async (params, { env }) => {
+    foo: (async () => {
       const dataset = env.dataset()
       const foo = env.namedNode('http://example.org/foo')
       const schemaTitle = env.namedNode(expand('schema:title')!)
