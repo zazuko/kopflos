03c1040a5a8e406c489a2e47f33778fab7951598
tpluscode <tpluscode@users.noreply.github.com>
2026-07-28 12:49:36 +0200

    test(plugin-pages): check patterns with and without trailing slash in base URI

--- labs/pages/test/lib/Plugin.test.ts
+++ labs/pages/test/lib/Plugin.test.ts
@@ -1,13 +1,13 @@
-import { resolve } from 'node:path'
-import { existsSync } from 'node:fs'
+import {resolve} from 'node:path'
+import {existsSync} from 'node:fs'
 import * as fs from 'node:fs/promises'
 import url from 'node:url'
-import { glob } from 'glob'
-import { expect, use } from 'chai'
-import type { Kopflos, KopflosEnvironment } from '@kopflos-cms/core'
-import { createEnv } from '@kopflos-cms/core/env.js'
+import {glob} from 'glob'
+import {expect, use} from 'chai'
+import type {Kopflos, KopflosEnvironment} from '@kopflos-cms/core'
+import {createEnv} from '@kopflos-cms/core/env.js'
 import snapshots from 'mocha-chai-rdf/snapshots.js'
-import type { Dataset } from '@zazuko/env/lib/DatasetExt.js'
+import type {Dataset} from '@zazuko/env/lib/DatasetExt.js'
 import formatsPretty from '@rdfjs-elements/formats-pretty'
 import DefaultPlugin from '@kopflos-cms/vite'
 import PagesPlugin from '../../lib/Plugin.js'
@@ -21,96 +21,106 @@ describe('@kopflos-cms/pages/lib/Plugin.js', function () {
     use(snapshots)
   })
 
-  beforeEach(function () {
-    env = createEnv({
-      baseIri: 'https://example.org/',
-      sparql: {
-        default: 'https://example.org/sparql',
-      },
-      buildDir,
-    }, process.cwd())
+  ;[
+    ['https://example.org/', 'base with slash'],
+    ['https://example.org', 'base without slash']
+  ].map(([baseIri, title]) => {
+    describe(title, () => {
+      beforeEach(function () {
+        env = createEnv({
+          baseIri,
+          sparql: {
+            default: 'https://example.org/sparql',
+          },
+          buildDir,
+        }, process.cwd())
 
-    env.formats.import(formatsPretty)
-  })
-
-  describe('apiGraphs', function () {
-    it('should return kopflos pages graph', function () {
-      // given
-      const plugin = new PagesPlugin({})
-      const kopflos = {
-        env,
-      } as unknown as Kopflos
-
-      // when
-      const graphs = plugin.apiGraphs(kopflos)
-
-      // then
-      expect(graphs).to.contain('https://kopflos.described.at/Pages')
-    })
-  })
-
-  describe('deployedResources', function () {
-    it('should generate resource shapes for pages', async function () {
-      // given
-      const plugin = new PagesPlugin({
-        path: 'test/fixtures/pages',
+        env.formats.import(formatsPretty)
       })
 
-      // when
-      const dataset = await plugin.deployedResources(env) as Dataset
+      describe('apiGraphs', function () {
+        it('should return kopflos pages graph', function () {
+          // given
+          const plugin = new PagesPlugin({})
+          const kopflos = {
+            env,
+          } as unknown as Kopflos
 
-      // then
-      expect(await dataset.serialize({ format: 'text/turtle' })).toMatchSnapshot()
-    })
-  })
+          // when
+          const graphs = plugin.apiGraphs(kopflos)
 
-  describe('watchPaths', function () {
-    it('should return configured path', function () {
-      // given
-      const plugin = new PagesPlugin({
-        path: 'my-pages',
+          // then
+          expect(graphs).to.contain('https://kopflos.described.at/Pages')
+        })
       })
 
-      // when
-      const paths = plugin.watchPaths()
+      describe('deployedResources', function () {
+        it('should generate resource shapes for pages', async function () {
+          // given
+          const plugin = new PagesPlugin({
+            path: 'test/fixtures/pages',
+          })
 
-      // then
-      expect(paths).to.contain('my-pages')
-    })
-  })
+          // when
+          const dataset = await plugin.deployedResources(env) as Dataset
 
-  describe('build', function () {
-    this.timeout(20000)
-
-    beforeEach(async function () {
-      await fs.rm(buildDir, { recursive: true, force: true })
-    })
-
-    it('should create build artifacts for pages', async function () {
-      // given
-      const plugin = new PagesPlugin({
-        path: 'test/fixtures/pages',
+          // then
+          expect(await dataset.serialize({format: 'text/turtle'})).toMatchSnapshot()
+        })
       })
-      const defaultPlugin = new DefaultPlugin()
 
-      // when
-      await plugin.build(env, [plugin, defaultPlugin])
+      describe('watchPaths', function () {
+        it('should return configured path', function () {
+          // given
+          const plugin = new PagesPlugin({
+            path: 'my-pages',
+          })
 
-      // then
-      const clientHtml = resolve(buildDir, 'test/fixtures/pages/client/test-page.html')
-      const serverJs = resolve(buildDir, 'test/fixtures/pages/server/test-page.js')
-      expect(existsSync(clientHtml)).to.be.true
-      expect(existsSync(serverJs)).to.be.true
+          // when
+          const paths = plugin.watchPaths()
 
-      const serverJsContent = await fs.readFile(serverJs, 'utf-8')
-      expect(serverJsContent).to.contain('test-page')
+          // then
+          expect(paths).to.contain('my-pages')
+        })
+      })
 
-      const serverDepsJs = resolve(buildDir, 'test/fixtures/pages/server/test-page.html.js')
-      expect(existsSync(serverDepsJs)).to.be.true
+      describe('build', function () {
+        this.timeout(20000)
 
-      const [clientJs] = await glob('test/fixtures/pages/client/assets/test-page-*.js', { cwd: buildDir, absolute: true })
-      const clientJsContent = await fs.readFile(clientJs, 'utf-8')
-      expect(clientJsContent).to.match(/console\.log\(['"`]test-page\.client\.ts['"`]\)/)
+        beforeEach(async function () {
+          await fs.rm(buildDir, {recursive: true, force: true})
+        })
+
+        it('should create build artifacts for pages', async function () {
+          // given
+          const plugin = new PagesPlugin({
+            path: 'test/fixtures/pages',
+          })
+          const defaultPlugin = new DefaultPlugin()
+
+          // when
+          await plugin.build(env, [plugin, defaultPlugin])
+
+          // then
+          const clientHtml = resolve(buildDir, 'test/fixtures/pages/client/test-page.html')
+          const serverJs = resolve(buildDir, 'test/fixtures/pages/server/test-page.js')
+          expect(existsSync(clientHtml)).to.be.true
+          expect(existsSync(serverJs)).to.be.true
+
+          const serverJsContent = await fs.readFile(serverJs, 'utf-8')
+          expect(serverJsContent).to.contain('test-page')
+
+          const serverDepsJs = resolve(buildDir, 'test/fixtures/pages/server/test-page.html.js')
+          expect(existsSync(serverDepsJs)).to.be.true
+
+          const [clientJs] = await glob('test/fixtures/pages/client/assets/test-page-*.js', {
+            cwd: buildDir,
+            absolute: true
+          })
+          const clientJsContent = await fs.readFile(clientJs, 'utf-8')
+          expect(clientJsContent).to.match(/console\.log\(['"`]test-page\.client\.ts['"`]\)/)
+        })
+      })
     })
   })
 })
